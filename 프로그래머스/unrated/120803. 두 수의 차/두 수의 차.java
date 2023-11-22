930edb23485a60822214d230a7e7537915ff58b9
이재현 <142370408+LeeJaeHyean@users.noreply.github.com>
2023-11-22 15:29:11 +0900

    [level unrated] Title: 두 수의 차, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+    
+    public static void main(String[] arg) {
+        System.out.println(solution(2,3));
+        System.out.println(solution(100,2));
+    }
+}
