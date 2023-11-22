dbd604cd7cc23a313c1dec4b6560ad995783f6ea
이재현 <142370408+LeeJaeHyean@users.noreply.github.com>
2023-11-22 15:19:26 +0900

    [level unrated] Title: 몫 구하기, Time: 0.02 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1/num2;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(solution(10,5));
+        System.out.println(solution(7,2));
+    }
+}
