342a7da0502ecbd64009c2b0394f76504ca6b6f5
이재현 <142370408+LeeJaeHyean@users.noreply.github.com>
2023-11-22 15:27:56 +0900

    [level unrated] Title: 나이 출력, Time: 0.03 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int age) {
+        return 2023-age;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(solution(40));
+        System.out.println(solution(23));
+    }
+}
