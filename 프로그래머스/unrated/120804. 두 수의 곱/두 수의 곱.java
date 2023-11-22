5703dd61725c0ba76c4d41c664957cc16865a933
이재현 <142370408+LeeJaeHyean@users.noreply.github.com>
2023-11-22 15:22:52 +0900

    [level unrated] Title: 두 수의 곱, Time: 0.01 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int solution(int num1, int num2) {
+        return num1 * num2;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(solution(3,4));
+        System.out.println(solution(27,19));
+    }
+}
